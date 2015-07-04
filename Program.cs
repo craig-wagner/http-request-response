eaf4bcdb9e56aa476b38d104cf0bbeac6dbc9aba
Craig Wagner <craig.wagner@gmail.com>
2015-07-04 13:54:42 -0700

    Initial

---
+++ Program.cs
@@ -0,0 +1,21 @@
+#region using
+using System;
+using System.Windows.Forms;
+#endregion using
+
+namespace HttpChecker
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault( false );
+            Application.Run( new MainForm() );
+        }
+    }
+}
